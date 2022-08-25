2852e4f8a3319eced0c21721ba91b55e59fa35ff
Moonzies <107804495+Moonzies@users.noreply.github.com>
2022-08-25 20:14:10 +0200

    Update raffleWhitelist.ts

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -7,7 +7,7 @@ const testWhitelist = new Map<string, RaffleMetaData>([
   [
     '3u7t4uGkMP1VW5obT78rgk74zdATTuNuy6BWPfRucLfj',
     {
-      name: 'for the win',
+      name: 'dRaffle for the win',
       alternatePurchaseMints: [
         new PublicKey('72UgZ7avdJZBbv3wR7hbWcFy6dyHHNAoJw7CimGA55Zh'),
       ],
@@ -15,12 +15,12 @@ const testWhitelist = new Map<string, RaffleMetaData>([
   ],
   [
     '2mrwjEz67DXTWKaVPjWwkwVPtGRWyKrTeyK5VzintsC5',
-    { name: 'loooooooooooooooong name' },
+    { name: 'dRaffle with a loooooooooooooooong name' },
   ],
   [
     '9FoUjfUpWwhHYaGKM9G5eYab7qow3oWqdo2G5Ehj3h5L',
     {
-      name: 'Raffle',
+      name: 'Oh my dRaffle',
       overviewImageUri: '/resources/001-mainnet-launch.gif',
       alternatePurchaseMints: [
         new PublicKey('So11111111111111111111111111111111111111112'),
@@ -29,7 +29,17 @@ const testWhitelist = new Map<string, RaffleMetaData>([
   ],
 ]);
 
+const prodWhitelist = new Map<string, RaffleMetaData>([
+  [
+    '9b6G6hWQvrmKwjgMXG3hhLduBzsnb4z4Qv3',
+    {
+      name: 'Updated soon',
+      overviewImageUri: '/resources/001-mainnet-launch.gif',
+    },
+  ],
 
+
+]);
 
 export const RAFFLES_WHITELIST = TESTING
   ? testWhitelist
