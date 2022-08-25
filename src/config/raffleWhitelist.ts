9f3681eb2173d1aa4b8d11298aa113f64b3cfdc9
Moonzies <107804495+Moonzies@users.noreply.github.com>
2022-08-25 18:56:47 +0200

    Update raffleWhitelist.ts

--- src/config/raffleWhitelist.ts
+++ src/config/raffleWhitelist.ts
@@ -7,7 +7,7 @@ const testWhitelist = new Map<string, RaffleMetaData>([
   [
     '3u7t4uGkMP1VW5obT78rgk74zdATTuNuy6BWPfRucLfj',
     {
-      name: 'dRaffle for the win',
+      name: 'for the win',
       alternatePurchaseMints: [
         new PublicKey('72UgZ7avdJZBbv3wR7hbWcFy6dyHHNAoJw7CimGA55Zh'),
       ],
@@ -15,12 +15,12 @@ const testWhitelist = new Map<string, RaffleMetaData>([
   ],
   [
     '2mrwjEz67DXTWKaVPjWwkwVPtGRWyKrTeyK5VzintsC5',
-    { name: 'dRaffle with a loooooooooooooooong name' },
+    { name: 'loooooooooooooooong name' },
   ],
   [
     '9FoUjfUpWwhHYaGKM9G5eYab7qow3oWqdo2G5Ehj3h5L',
     {
-      name: 'Oh my dRaffle',
+      name: 'Raffle',
       overviewImageUri: '/resources/001-mainnet-launch.gif',
       alternatePurchaseMints: [
         new PublicKey('So11111111111111111111111111111111111111112'),
@@ -29,44 +29,7 @@ const testWhitelist = new Map<string, RaffleMetaData>([
   ],
 ]);
 
-const prodWhitelist = new Map<string, RaffleMetaData>([
-  [
-    '9UHPUYJ5zWb6G6hWQvrmKwjgMXG3hhLduBzsnb4z4Qv3',
-    {
-      name: 'My First Raffle Thanks to Domo',
-      overviewImageUri: '/resources/001-mainnet-launch.gif',
-    },
-  ],
-  [
-    'BkfzyRveM4hFtgh93T9qPMAt8kS8xbA8knATaTurNYVf',
-    {
-      name: '3 Prizes Test',
-      overviewImageUri: '/resources/aartist-raffle-overview.gif',
-    },
-  ],
-  [
-    'BkfzyRveM4hFtgh93T9qPMAt8kS8xbA8knATaTurNYVf',
-    {
-      name: '3 Prizes Test',
-      overviewImageUri: '/resources/aartist-raffle-overview.gif',
-    },
-  ],
-  [
-    'BkfzyRveM4hFtgh93T9qPMAt8kS8xbA8knATaTurNYVf',
-    {
-      name: '3 Prizes Test',
-      overviewImageUri: '/resources/aartist-raffle-overview.gif',
-    },
-  ],
-  [
-    '9BtoNqpyqF7Sippz5uwUCCgZCoJ7Pv2Z1GdhUi3edDaT',
-    {
-      name: 'Oliver is King',
-      overviewImageUri: '/resources/aartist-raffle-overview.gif',
-    },
-  ],
 
-]);
 
 export const RAFFLES_WHITELIST = TESTING
   ? testWhitelist
